44af950462c35d3026e44860970bc2354797450d
Eduardo Felix <dudumacedofelix613@gmail.com>
2022-09-26 15:34:46 -0300

    Testando site PNL

--- Py-TesteNPL/teste.py
+++ Py-TesteNPL/teste.py
@@ -26,8 +26,10 @@ driver.switch_to.frame("iframe_MenuPrincipal")
 driver.switch_to.frame("nmsc_iframe_IframeZoho_1")
 driver.switch_to.frame(driver.find_element(By.XPATH, "//iframe[@src='https://reports.zoho.com/ZDBDataSheetView.cc?OBJID=1390984000000007513&STANDALONE=true&WIDTH=800&HEIGHT=600&INTERVAL=-1&REMTOOLBAR=true&INCLUDETITLE=true&INCLUDEDESC=true']"))
 wait.until(EC.visibility_of_element_located((By.XPATH, "//td[.='Campus:']/../../tr/td/div/div[@value='Todos']")))
+
 btn_todos = driver.find_element(By.XPATH, "//td[.='Campus:']/../../tr/td/div/div[@value='Todos']/div").click()
 
+btn_check_todos = driver.find_element(By.XPATH, '//div[@id="vmOptionList_Campus"]/div[@elname="optionDiv"]/ul[@class="multi_allOpt VMFilterValue VMFilterValueNew VMFilterValueMulti"]/li/span[@class="likeCheckBoxEl"]').click()
 # driver.switch_to.default_content()
 
 # driver.quit()
